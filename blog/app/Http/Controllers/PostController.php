177cfe12f346104e23737f07d8a3d257a226f7b2
ishimurasatoshi <sato0315ishi524n@yahoo.co.jp>
2022-09-05 18:21:35 +0900

    dev_basis03_2

--- blog/app/Http/Controllers/PostController.php
+++ blog/app/Http/Controllers/PostController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+    * 特定IDのpostを表示する
+    *
+    * @params Object Post // 引数の$postはid=1のPostインスタンス
+    * @return Reposnse post view
+    */
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+    
 }
