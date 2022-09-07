24e9e96cf7488e3c97104e57da16f0c1ae37cd65
ishimurasatoshi <sato0315ishi524n@yahoo.co.jp>
2022-09-08 06:10:45 +0900

    dev_basis06

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -13,6 +13,11 @@
             {{ $post->title }}
         </h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+            @csrf
+            @method('DELETE')
+            <button type="submit" onclick="deletePost(this)">delete</button> 
+        </form>
         <div class="content">
             <div class="content__post">
                 <h3>本文</h3>
@@ -23,6 +28,14 @@
         <div class="footer">
             <a href="/">戻る</a>
         </div>
+        <script>
+            function deletePost(e){
+                'use strict'
+                if(comfirm('削除すると復元できません。\n本当に削除しますか？')){
+                    document.getElementById('form_delet').submit();
+                }
+            }
+        </script>
     </body>
 </html>
     
